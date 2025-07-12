0a222c9e0db72f8882e750310b13db72ded11d73
Ali Khorsandfard <secure73@gmail.com>
2025-07-12 12:01:54 +0200

    vite config

--- src/setup/vite.config.js
+++ src/setup/vite.config.js
@@ -4,17 +4,20 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'assets/js',
+  root: 'assets',
   build: {
-    outDir: '../build',
+    outDir: '../public/assets/build',
     emptyOutDir: true,
+    manifest: true,
     rollupOptions: {
       input: {
         app: resolve(__dirname, 'assets/js/app.jsx'),
       },
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: 'css/[name].[ext]',
       },
     },
   },
-}); 
+});
